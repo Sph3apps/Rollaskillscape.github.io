6c7e61341512e3cba1a7426d6325767fc9523e76
Sph3apps <sph3apps@hotmail.com>
2025-10-25 03:55:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "Rollaskillscape.github.io"
+  base: '/Rollaskillscape.github.io/', // 👈 must match your repo name exactly
   plugins: [react()],
 })
