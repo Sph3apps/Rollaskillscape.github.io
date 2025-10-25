ccdbf80542bb5bad692d7b838b40ed65df794806
Sph3apps <sph3apps@hotmail.com>
2025-10-25 03:06:14 +0100

    Delete src directory

--- src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p372506497676984320',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
-
