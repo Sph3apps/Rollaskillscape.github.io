77b833f1fc80064c169ade6829ab4f971cf276c1
Sph3apps <sph3apps@hotmail.com>
2025-11-03 11:27:32 +0000

    Update GamePanel.jsx

--- src/components/GamePanel.jsx
+++ src/components/GamePanel.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // ✅ Added import
+import { Link } from 'react-router-dom'; 
 
 const GamePanel = ({
   onBackToWelcome,
